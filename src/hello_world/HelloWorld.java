e53771807761299cdadc5d24a58d2c3fc6337c20
V+ <wikka9088@gmail.com>
2018-04-09 23:57:58 -0400

    i am back

---
+++ src/hello_world/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello_world;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int s = 0; s < 10; s++) {
+			System.out.println("Hello World!");
+		}
+
+	}
+
+}
